50d79b37b9c958469fd033de0012e71da4512e23
pbone64 <47093312+pbone64@users.noreply.github.com>
2024-07-28 21:30:05 -0500

    Add Target.__init__

--- wobsite/compiler/targets.py
+++ wobsite/compiler/targets.py
@@ -12,8 +12,14 @@ class CompiledWobsite:
 IN = TypeVar("IN", contravariant=True)
 OUT = TypeVar("OUT", covariant=True)
 class Target(Generic[IN, OUT]):
+    def __init__(self, input: "Target[object, IN]") -> None:
+        self.input = input
+
     resolve: Callable[["Target[object, IN]", WobsiteCompilation], OUT]
     input: "Target[object, IN]"
+
+    def get(self, ctx: WobsiteCompilation) -> OUT:
+        return self.resolve(self.input, ctx)
 
 class RootTarget(Generic[IN], Target[IN, None]):
     pass
